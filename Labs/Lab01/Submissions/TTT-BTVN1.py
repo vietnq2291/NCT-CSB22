1374d0dae3d25c06aaf28ef42864a784deeff703
unknown <trungtin7707@gmail.com>
2023-04-23 07:30:54 +0700

    BTVN - 1

---
+++ Labs/Lab01/Submissions/TTT-BTVN1.py
@@ -0,0 +1,98 @@
+import turtle
+#BT1: 
+name = input("Input your name: ")
+print(f'Welcome, {name}!')
+
+#BT2: 
+firstName = input("First Name: ")
+lastName = input("Last Name: ")
+phoneNum = input("Phone number: ")
+print(f"Your registered name is {firstName} {lastName}.")
+print(f"Your phone number is {phoneNum}")
+
+#BT3: 
+print(f"Name\t\t:\t{firstName} {lastName}\nBirthdate\t:\t07/06/2000")
+
+#BT4:
+yearInput = input("What year it is?\n>>")
+
+print(f"\t\t  . : .\n\t\t '. : .'\nHAPPY NEW YEAR\t. '.:.' .\n !!! {yearInput} !!!   . .':'. .\n\t\t .' : '.\n\t\t  ' : '")
+
+
+#BT5: 
+"""
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(50)
+turtle.left(90)
+"""
+
+#BT6: 
+"""
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.penup()
+turtle.forward(20)
+turtle.pendown()
+turtle.pensize(5)
+turtle.left(90)
+turtle.forward(120)
+turtle.left(90)
+turtle.forward(140)
+turtle.left(90)
+turtle.forward(140)
+turtle.left(90)
+turtle.forward(140)
+turtle.left(90)
+turtle.forward(70)
+turtle.left(90)
+turtle.penup()
+turtle.forward(70)
+"""
+
+"""
+#BT7:
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.penup()
+turtle.forward(120)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(45)
+turtle.pendown()
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.penup()
+turtle.left(135)
+turtle.forward(70)
+turtle.right(90)
+"""
+
+
+"""
+turtle.right()
+turtle.forward()
+turtle.backward()
+"""
+
+
