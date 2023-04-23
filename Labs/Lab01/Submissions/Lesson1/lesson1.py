93a09b0b074e398fd07e2e4c8de6d8846a481a7c
HaiTrieu69 <115881454+HaiTrieu69@users.noreply.github.com>
2023-04-23 15:17:35 +0700

    Add files via upload
    btvn

---
+++ Labs/Lab01/Submissions/Lesson1/lesson1.py
@@ -0,0 +1,114 @@
+#Bài 1:
+print("Đây là bài số 1")
+name = input('Input your name:')
+print("Welcome", name,("!"))
+
+
+
+#Bài 2:
+print("Đây là bài số 2")
+First_name = input("Input your first name:")
+Last_name = input("Input your last name:")
+Phone_number = input("Input your phone number:")
+print("Your registered name is", First_name, Last_name)
+print("Your phone number is", Phone_number)
+
+
+#Bài 3:
+print("Đây là bài số 3")
+name = input('Input your name:')
+birthday = input("Input your birthday (example: 18/06/2006): ")
+print("Name\t\t:\t\t", name)
+print("Birthday\t\t:\t\t", birthday)
+
+
+
+
+#Bài 5:
+print("Đây là bài số 5")
+import turtle
+t = turtle.Turtle()
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(50)
+t.left(90)
+turtle.mainloop()
+
+
+#Bài 6:
+print("Đây là bài số 6:")
+import turtle
+t = turtle.Turtle()
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.penup()
+t.forward(20)
+t.pendown()
+t.pensize(5)
+t.left(90)
+t.forward(120)
+t.left(90)
+t.forward(140)
+t.left(90)
+t.forward(140)
+t.left(90)
+t.forward(140)
+t.left(90)
+t.forward(70)
+t.left(90)
+t.penup()
+t.forward(70)
+turtle.mainloop()
+
+
+#Bài 7
+print("Đây là bài số 7")
+import turtle
+t = turtle.Turtle()
+t.left(360)
+t.forward(100)
+t.right(90)
+t.left(360)
+t.forward(100)
+t.right(90)
+t.left(360)
+t.forward(100)
+t.right(90)
+t.left(360)
+t.forward(100)
+t.right(90)
+t.left(360)
+t.forward(50)
+t.left(90)
+t.penup()
+t.left(360)
+t.forward(30)
+t.left(135)
+t.pendown()
+t.left(360)
+t.forward(120)
+t.left(90)
+t.left(360)
+t.forward(120)
+t.left(90)
+t.left(360)
+t.forward(120)
+t.left(90)
+t.left(360)
+t.forward(120)
+t.left(135)
+t.penup()
+t.left(360)
+t.forward(80)
+t.left(360)
+t.right(360)
+turtle.mainloop()
