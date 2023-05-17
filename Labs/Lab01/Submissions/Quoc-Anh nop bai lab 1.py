2fb3938a03910525acbf92d1221974ff01cee107
quocanh10cnn <131584448+quocanh10cnn@users.noreply.github.com>
2023-05-17 21:48:58 +0700

    Add files via upload

---
+++ Labs/Lab01/Submissions/Quoc-Anh nop bai lab 1.py
@@ -0,0 +1,100 @@
+#BT1
+name = input ("Input your name: ")
+print ("Welcome,", name, "!")
+
+#BT2
+First_name = input("First name: ")
+Last_name = input("Last name: ")
+Phone_number = input("Phone number: ")
+print("Your registered name is:", First_name + Last_name, ".")
+print("Your phone number is:", Phone_number, ".")
+
+# BT3
+print("Name\t\t:\tQuốc Anh\nBirthdate\t:\t26/07/2006")
+
+# BT4
+Year = input("What year is it?\n")
+print("HAPPY NEW YEAR\n !!!", Year,"!!!")
+
+# BT5
+from turtle import *
+
+t = Turtle() 
+t.right (180)
+t.forward (100)
+t.right (120)
+t.forward (200)
+t.right (120)
+t.forward (200)
+t.right (120)
+t.forward (100)
+t.right (90)
+
+#BT6
+from turtle import *
+
+t = Turtle() 
+t.pensize (5)
+t.forward (100)
+t.left (90)
+t.forward (200)
+t.left (90)
+t.forward (200)
+t.left (90)
+t.forward (200)
+t.left (90)
+t.forward (100)
+t.penup ()
+t.forward (80)
+t.left (90)
+t.pensize (0.5)
+t.forward (20)
+t.pendown ()
+t.forward (160)
+t.left (90)
+t.forward (160)
+t.left (90)
+t.forward (160)
+t.left (90)
+t.forward (160)
+t.penup ()
+t.backward (80)
+t.left (90)
+t.forward (80)
+
+#BT7
+from turtle import *
+
+
+t = Turtle() 
+
+t.penup ()
+t.forward (150)
+t.left (135)
+t.pendown ()
+t.forward (200)
+t.left (90)
+t.forward (200)
+t.left (90)
+t.forward (200)
+t.left (90)
+t.forward (200)
+t.left (135)
+t.penup ()
+t. pensize (3)
+t.forward (40)
+t.right (90)
+t.pendown ()
+t. forward (100)
+t.left (90)
+t.forward (200)
+t.left (90)
+t.forward (200)
+t.left (90)
+t.forward (200)
+t.left (90)
+t.forward (100)
+t.penup ()
+t.left (90)
+t.forward (100)
+t.right (90)
