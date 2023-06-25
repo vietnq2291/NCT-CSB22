015c635cf3336ca38cebaa9dfed017dc2455dc7e
vietnq2291 <viet.nq194881@sis.hust.edu.vn>
2023-06-25 16:51:03 +0700

    add lesson 10 in class exercise

---
+++ Lesson10/exercises.py
@@ -0,0 +1,47 @@
+def ex1():
+    keys = ['Ten', 'Twenty', 'Thirty']
+    values = [10, 20, 30]
+
+    return # {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+
+def ex2():
+    dict1 = {'Ten': 10, 'Twenty': 20, 'Thirty': 30}
+    dict2 = {'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+    return # {'Ten': 10, 'Twenty': 20, 'Thirty': 30, 'Fourty': 40, 'Fifty': 50}
+
+def ex3():
+    sampleDict = {
+    "class": {
+        "student": {
+            "name": "Mike",
+            "marks": {
+                "physics": 70,
+                "history": 80
+                }
+            }
+        }
+    }
+
+    # print value of key 'history'
+
+def ex4():
+    sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+    # Keys to extract
+    keys = ["name", "salary"]
+
+    return # a dictonary which has keys in keys list and values inferred from sample_dict
+
+def ex5():
+    sample_dict = {
+        'Physics': 82,
+        'Math': 65,
+        'history': 75
+    }
+
+    return # key of the mimimum value in the dict
